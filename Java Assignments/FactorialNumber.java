385e022916d9d470a45daa645249485a873b9a60
rupesh1010 <76780243+rupesh1010@users.noreply.github.com>
2021-01-05 16:12:25 +0530

    Update FactorialNumber.java

--- Java Assignments/FactorialNumber.java
+++ Java Assignments/FactorialNumber.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 class FactorialNumber{
-	public static void main(String args[]){
+	public static void main(String args[])
+        {
 		System.out.print("Enter any Number: ");
 			Scanner in=new Scanner(System.in);
 			int n,fact=1;
 			n=in.nextInt();
-			int	i=1;
+			int i=1;
 			while(i<=n)
 			{
 				fact=fact*i;
